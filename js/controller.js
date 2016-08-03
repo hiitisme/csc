0545d1f1aa89006388266991bc0db2ea0f8be4e8
Pranavadurai  Samidurai <psamidurai@asiapac.globalcsc.net>
2016-08-03 11:38:57 +0530

    first version

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+var CscControl = angular.module("CscControl",[]);
+CscControl.controller("cscctrl",function($scope){
+
+
+    $scope.contacts = [
+      {name:'Pranav',no:'121211'},
+      {name:'venkat',no:'121211'},
+      {name:'aravindh',no:'121211'},
+      {name:'srudeep',no:'12112111'},
+      {name:'paddy',no:'121212122'}];
+
+
+
+
+});
